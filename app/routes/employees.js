d58c2f96c974e793b87233a2ab3a8d4b86a92945
Yuichi Hagio <yhagio87@gmail.com>
2016-07-17 10:36:36 -0400

    Employee page responsive map

--- app/routes/employees.js
+++ app/routes/employees.js
@@ -49,26 +49,65 @@ function createGeoView (jsonData) {
     (employee.location === 'Orlando') ? orlando++ : null;
   });
 
-  // google.charts.load('current', {'packages': ['geomap', 'corechart']});
-   
+  google.charts.setOnLoadCallback(drawMap);
+  google.charts.setOnLoadCallback(drawChart);
+
 
   // Draw Geographic Map Chart of Employees
   function drawMap() {
-    var data = google.visualization.arrayToDataTable([
-      ['City', 'Number of Employees'],
-      ['Boston', boston],
-      ['Orlando', orlando],
-      ['Chicago', chicago],
-      ['SanFrancisco', sanfran]
-    ]);
 
-    var options = {};
-    options['region'] = 'US';
-    options['colors'] = [0xFF8747, 0xFFB581, 0xc06000];
-    options['dataMode'] = 'markers';
-    var geomap = new google.visualization.GeoMap(document.getElementById('map-canvas'));
-    geomap.draw(data, options);
+    setTimeout(() => {
+      let bounds = new google.maps.LatLngBounds();
+      const USA = {lat: 37.09024, lng: -95.712891};
+      const map = new google.maps.Map(document.getElementById("map-canvas"), {
+        zoom: 3,
+        center: USA
+      });
+
+      // Markers array
+      const markers = [
+        ['Boston', boston, 42.3135417, -71.1975856],
+        ['Chicago', chicago, 41.8339026, -88.0130316],
+        ['San Francisco', sanfran, 37.7578149, -122.507812],
+        ['Orlando', orlando, 28.4813986, -81.5091802]
+      ];
+
+      // Info Content array of each marker
+      function infoWindowContent(name, num) {
+        return `<div class="markerInfo"><h5>${name}</h5><p>Number of employees: ${num}</p></div>`;
+      };
+
+      // Display multiple markers on a map
+      let infoWindow = new google.maps.InfoWindow();
+      let marker;
+
+      for (let i = 0; markers.length > i; i++) {
+        let position = new google.maps.LatLng(markers[i][2], markers[i][3]);
+        bounds.extend(position);
+
+        // Setting each marker location
+        marker = new google.maps.Marker({
+          position,
+          map,
+          title: markers[i][0]
+        });
+
+        // Setting each marker's info window
+        google.maps.event.addListener(marker, 'click', ((marker, i) => {
+          return () => {
+            infoWindow.setContent(infoWindowContent(markers[i][0], markers[i][1]));
+            infoWindow.open(map, marker);
+          }
+        })(marker, i));
+
+        // Automatically center the map fitting all markers on the screen
+        map.fitBounds(bounds);
+      }
+
+    }, 300); // ** For avoiding failing to find the element to attach the map, wait 300ms
+
   }
+
 
   // Draw Donut Chart of Employees
   function drawChart() {
@@ -90,10 +129,8 @@ function createGeoView (jsonData) {
     chart.draw(data, options);
   }
 
-  google.charts.setOnLoadCallback(drawChart);
-  google.charts.setOnLoadCallback(drawMap);
-
   $(window).resize(function(){
     drawChart();
+    drawMap();
   });
 }
